9f75f3ff805c1112e718689306c8f04c4ebd850f
lindennerd <lindennerd@gmail.com>
2022-09-11 17:22:11 -0300

    client side

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,8 +5,12 @@ import { env } from "./env";
 import { resolvers } from "./schema/resolvers";
 import { typeDefs } from "./schema/type-defs";
 
+import morgan from "morgan";
+
 const app = express();
 app.use(express.json());
+
+app.use(morgan("dev"));
 
 app.use(
   env.graphqlPath,
--- api/src/lib/image.ts
+++ api/src/lib/image.ts
@@ -3,12 +3,7 @@ import { serverPath } from "../server-path";
 
 export const image = async (text: string) => {
   const image = await Jimp.read(serverPath("/assets/olavo-post.jpg"));
-
-  console.log("image", image);
-
   const loadedFont = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  console.log("loaded font", loadedFont);
-
   const printedImage = await image.print(loadedFont, 10, 90, text, 700, 100);
   const imgUrl = await printedImage.getBase64Async("image/jpeg");
 
